a50597cb2c8103caed7b49dcc6cb73001c06b552
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-07-08 22:43:07 +0630

    Create adpad

---
+++ README.md
@@ -0,0 +1,19 @@
+## Pretend as D-Pad for Android TV 
+
+This won't work for every keystrokes, I mainly use as a D-pad for Android devices. While developing Android TV Apps.
+I'm sick of using the remote control or laggy virtual D-pad on the screen. -_-
+
+## Requirements
+
+- C Compiler
+- adb
+
+> And of course, you need to connect your Android device to your computer via adb first.
+
+## Usage
+
+Compile with your favorite C compiler and run the binary.
+
+## Disclaimer
+
+> This is a quick and dirty hack, so don't expect too much. I spent less than 10 minutes on this. :3
---
+++ adpad.c
@@ -0,0 +1,86 @@
+// Shamelessly copy pasted how to listen to keyboard events in C
+// from https://stackoverflow.com/a/29723614/14837948 😅
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+static struct termios oldSettings;
+
+void kbcleanup(void) {
+  tcsetattr(0, TCSAFLUSH, &oldSettings); /* restore old settings */
+}
+
+void kbsetup(void) {
+  tcgetattr(0, &oldSettings);
+
+  struct termios newSettings = oldSettings;
+
+  newSettings.c_lflag &= ~ICANON; /* disable line-at-a-time input */
+  newSettings.c_lflag &= ~ECHO;   /* disable echo */
+  newSettings.c_cc[VMIN] = 0;     /* don't wait for characters */
+  newSettings.c_cc[VTIME] = 0;    /* no minimum wait time */
+
+  if (tcsetattr(0, TCSAFLUSH, &newSettings) == 0) {
+    atexit(
+        kbcleanup); /* restore the terminal settings when the program exits */
+  } else {
+    fprintf(stderr, "Unable to set terminal mode\n");
+    exit(1);
+  }
+}
+
+int getkey(void) {
+  char c;
+
+  if (read(STDIN_FILENO, &c, 1) == 0)
+    return '\0';
+  else
+    return c;
+}
+
+int main(void) {
+  int c;
+
+  kbsetup();
+
+  time_t start = time(NULL);
+  time_t previous = start;
+  for (;;) {
+    usleep(1000);
+    time_t current = time(NULL);
+
+    if (current != previous) {
+      previous = current;
+    } else if ((c = getkey()) != '\0') {
+      if (c == 27) {
+        c = getkey();
+        if (c == 91) {
+          c = getkey();
+          if (c == 65) {
+            system("adb shell input keyevent 19");
+          } else if (c == 66) {
+            system("adb shell input keyevent 20");
+          } else if (c == 67) {
+            system("adb shell input keyevent 22");
+          } else if (c == 68) {
+            system("adb shell input keyevent 21");
+          }
+        }
+      } else if (c == 10) {
+        system("adb shell input keyevent 66");
+      } else if (c == 127) {
+        system("adb shell input keyevent 67");
+      } else if (c == 32) {
+        system("adb shell input keyevent 62");
+      } else {
+        char command[100];
+        sprintf(command, "adb shell input text '%c'", c);
+        system(command);
+      }
+    }
+  }
+}
